84f791c81f7b764d6b14d0238efd945c068df017
Aryan Mehta <aryanmehta2016@gmail.com>
2024-05-30 13:56:15 +0100

    added static files and rendering page

---
+++ restaurant/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def sayhello(request):
+    return HttpResponse("Hello World!")
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
